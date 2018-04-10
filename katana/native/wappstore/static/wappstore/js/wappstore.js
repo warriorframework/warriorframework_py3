485fc515b13cf185ae3be9cbbf103f9dd10f6587
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2018-04-10 11:24:02 -0500

    Changes

--- katana/native/wappstore/static/wappstore/js/wappstore.js
+++ katana/native/wappstore/static/wappstore/js/wappstore.js
@@ -8,5 +8,17 @@ var wappstore = {
             }).done(function(data){
 
         });
+    },
+
+    collapseSubSidebar: function () {
+        var $elem = $(this);
+        var $subBar = $elem.closest('.wappstore-main-bar').next();
+        if ($elem.find('i').hasClass('fa-chevron-circle-up')){
+            $subBar.hide();
+            $elem.find('i').removeClass('fa-chevron-circle-up').addClass('fa-chevron-circle-down');
+        } else {
+            $subBar.show();
+            $elem.find('i').removeClass('fa-chevron-circle-down').addClass('fa-chevron-circle-up');
+        }
     }
 };
--- katana/native/wappstore/views.py
+++ katana/native/wappstore/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 from django.views import View
 import requests
 
+address_port = "http://localhost:5000"
+
 
 class WappStoreView(View):
 
@@ -15,10 +17,10 @@ class WappStoreView(View):
         """
         Get Request Method
         """
-        response = json.loads(requests.get('http://localhost:5000/wapps/get_all_wapps_data/').content)
+        response = json.loads(requests.get('{0}/wapps/get_all_wapps_data/'.format(address_port)).content)
         return render(request, WappStoreView.template, response)
 
 
 def expand_wapp(request):
-    response = json.loads(requests.get('http://localhost:5000/wapps/get_wapp_info/').content)
+    response = json.loads(requests.get('{0}/wapps/get_wapp_info/'.format(address_port)).content)
     return render(request, 'wappstore/expand_wapp.html', response)
