21f0fa6359641e9402df2a9ae6c8339871091c55
Venkatesh Sethuram <venkatesh.sethuram@us.fujitsu.com>
2018-04-20 14:07:04 +0530

    support for setting pseudo-terminal dimensions

--- warrior/Framework/ClassUtils/WNetwork/warrior_cli_class.py
+++ warrior/Framework/ClassUtils/WNetwork/warrior_cli_class.py
@@ -19,6 +19,8 @@ import subprocess
 import getpass
 import xml
 import Tools
+import ast
+from distutils.version import LooseVersion
 from Framework import Utils
 from Framework.Utils.print_Utils import print_info, print_debug,\
  print_warning, print_exception, print_error
@@ -855,15 +857,29 @@ class WarriorCli(object):
 
     @staticmethod
     def pexpect_spawn_with_env(pexpect_obj, command, timeout, escape=False,
-                               env=None):
+                               env=None, dimensions=None):
+        """ spawn a pexpect object with environment & dimensions variables """
 
-        """ spawn a pexpect object with environment variable """
-        if env is None:
+        if not(str(escape).lower() == "yes" or str(escape).lower() == "true"):
             env = {}
-        if str(escape).lower() == "yes" or str(escape).lower() == "true":
-            child = pexpect_obj.spawn(command, timeout=int(timeout), env=env)
+
+        sendDimensions = False
+        if dimensions is not None:
+            # 'dimensions' argument is supported in pexpect version 4.0 and above
+            if LooseVersion(pexpect_obj.__version__) >= LooseVersion('4.0'):
+                sendDimensions = True
+            else:
+                print_warning("Setting pseudo-terminal dimensions is not supported in "
+                              "pexpect versions less than 4.0(installed pexpect "
+                              "version: {}), 'dimensions' value will be default "
+                              "to None".format(pexpect_obj.__version__))
+
+        if sendDimensions is True:
+            child = pexpect_obj.spawn(command, timeout=int(timeout), env=env,
+                                      dimensions=dimensions)
         else:
-            child = pexpect_obj.spawn(command, timeout=int(timeout))
+            child = pexpect_obj.spawn(command, timeout=int(timeout), env=env)
+
         return child
 
     @staticmethod
@@ -1255,6 +1271,8 @@ class PexpectConnect(object):
                 10. escape(string) = true/false(to escape color codes by
                                      setting TERM as dump)
                 11. conn_type = session type(ssh/telnet)
+                12. dimensions(tuple) = size of the pseudo-terminal specified
+                                        as a two-entry tuple(rows, columns)
          """
 
         self.pexpect = None
@@ -1276,6 +1294,19 @@ class PexpectConnect(object):
         self.conn_options = credentials.get('conn_options', '')
         self.custom_keystroke = credentials.get('custom_keystroke', '')
         self.escape = credentials.get('escape', False)
+        self.dimensions = credentials.get('dimensions', None)
+        # convert dimension value from string to tuple
+        if self.dimensions and isinstance(self.dimensions, str):
+            err_msg = ("Invalid value '{}' given for dimensions argument, it "
+                       "only accepts tuple value(It will be default to None).")
+            try:
+                self.dimensions = ast.literal_eval(self.dimensions)
+            except Exception:
+                print_warning(err_msg.format(self.dimensions))
+            else:
+                if not isinstance(self.dimensions, tuple):
+                    print_warning(err_msg.format(self.dimensions))
+                    self.dimensions = None
 
     def __import_pexpect(self):
         """Import the pexpect module """
@@ -1321,7 +1352,9 @@ class PexpectConnect(object):
         print_debug("connectSSH: cmd = %s" % command)
         child = WarriorCli.pexpect_spawn_with_env(self.pexpect, command,
                                                   self.timeout,
-                                                  env={"TERM": "dumb"})
+                                                  escape=self.escape,
+                                                  env={"TERM": "dumb"},
+                                                  dimensions=self.dimensions)
 
         child.logfile = sys.stdout
 
@@ -1380,8 +1413,11 @@ class PexpectConnect(object):
                     print_debug("SSH Host Key is changed - Remove it from "
                                 "known_hosts file : cmd = %s" % cmd)
                     subprocess.call(cmd, shell=True)
-                    child = self.pexpect.spawn(command,
-                                               timeout=int(self.timeout))
+                    child = WarriorCli.pexpect_spawn_with_env(self.pexpect, command,
+                                                              self.timeout,
+                                                              escape=self.escape,
+                                                              env={"TERM": "dumb"},
+                                                              dimensions=self.dimensions)
                     print_debug("ReconnectSSH: cmd = %s" % command)
         except Exception as exception:
             print_exception(exception)
@@ -1408,7 +1444,9 @@ class PexpectConnect(object):
 
         child = WarriorCli.pexpect_spawn_with_env(self.pexpect, command,
                                                   self.timeout,
-                                                  env={"TERM": "dumb"})
+                                                  env={"TERM": "dumb"},
+                                                  escape=self.escape,
+                                                  dimensions=self.dimensions)
 
         try:
             child.logfile = open(self.logfile, "ab")
