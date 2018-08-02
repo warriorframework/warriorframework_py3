be1271e5bfa26624f588a6ee4f25296c15048758
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2018-08-02 15:08:33 -0500

    Edited license quotes

--- warrior/ProductDrivers/wapp_driver.py
+++ warrior/ProductDrivers/wapp_driver.py
@@ -1,4 +1,4 @@
-"""
+'''
 Copyright 2017, Fujitsu Network Communications, Inc.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,7 +10,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-"""
+'''
+
+""" Wapp Driver """
 
 from WarriorCore import kw_driver
 import Actions.WappActions.WappReportingActions
