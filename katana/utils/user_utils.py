562cf3e30f9bb2fbc4a6853633eb320da612788f
Bennur, Suraj <suraj.bennur@us.fujitsu.com>
2018-08-08 18:41:34 -0500

    +get_user_path() and changes to get_dotdata_dir(), get_wapplogs_dir()

--- katana/utils/user_utils.py
+++ katana/utils/user_utils.py
@@ -7,6 +7,8 @@ from .directory_traversal_utils import get_parent_dir_path, join_path, file_or_d
 
 DOTDATA = '.data'
 WAPPLOGS = 'wapp_logs'
+WAPPSDATA = 'wapps_data'
+USERDATA = 'user_data'
 
 
 class UserData:
@@ -15,46 +17,57 @@ class UserData:
         self.username = username
         self.wapp_name = wapp_name
 
+    def get_user_path(self, request):
+        username = user_authenticated(request)
+        if username is not None:
+            self.username = username
+            top_dir = get_user_home_dir(username)
+            if top_dir is not "":
+                user_dir = join_path(top_dir, USERDATA, username)
+                return user_dir
+            else:
+                print('fetching of top level directory resulted in a empty string')
+                return None
+        else:
+            print('user not authenticated')
+            return None
+
     # define method, get .data directory
     # given request object
     def get_dotdata_dir(self, request):
         # get location to .data
-        username = user_authenticated(request)
-        if username is not None and self.wapp_name is not None:
-            self.username = username
+        if self.wapp_name is not None:
             wapp_name = self.wapp_name
             # get top level directory for user_data
-            top_dir = get_user_home_dir(username)
-            if top_dir is not "":
-                dotdata_dir = join_path(top_dir, username, wapp_name, DOTDATA)
+            user_path = self.get_user_path(self, request)
+            if user_path is not None:
+                dotdata_dir = join_path(user_path, WAPPSDATA, wapp_name, DOTDATA)
                 if file_or_dir_exists(dotdata_dir):
                     return dotdata_dir
             else:
-                print('top_dir not found')
+                print('user_path not found')
                 return None
         else:
-            print('.data directory not found')
+            print('wapp_name not available')
             return None
 
     # define method, get wapp_logs
     # given request object
     def get_wapplogs_dir(self, request):
         # get location to wapp_logs
-        username = user_authenticated(request)
-        if username is not None and self.wapp_name is not None:
-            self.username = username
+        if self.wapp_name is not None:
             wapp_name = self.wapp_name
             # get top level directory for user_data
-            top_dir = get_user_home_dir(username)
-            if top_dir is not "":
-                wapplogs_dir = join_path(top_dir, username, wapp_name, WAPPLOGS)
+            user_path = self.get_user_path(self, request)
+            if user_path is not None:
+                wapplogs_dir = join_path(user_path, WAPPSDATA, wapp_name, WAPPLOGS)
                 if file_or_dir_exists(wapplogs_dir):
                     return wapplogs_dir
             else:
-                print('top_dir not found')
+                print('user_path not found')
                 return None
         else:
-            print('wapp_logs directory not found')
+            print('wapp_name not available')
             return None
 
 
