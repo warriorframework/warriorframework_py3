5f4bbca8f52cdbd00d9bbecf5ebe8645748a6a93
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2018-11-01 15:42:31 -0500

    Mostly done

--- katana/wui/core/static/core/js/main_django.js
+++ katana/wui/core/static/core/js/main_django.js
@@ -3,6 +3,12 @@ var katana = {
         toggleSidebar: function ($elem) {
             var $parent = $elem.closest('.page-content');
             $parent.toggleClass('expanded collapsed');
+        },
+
+        toggleSubMenu: function ($elem) {
+            var $page = $('body');
+            $page.find('#' + $elem.attr('target')).toggleClass('hidden');
+            $elem.toggleClass('fa-chevron-down fa-chevron-up');
         }
     }
 };
