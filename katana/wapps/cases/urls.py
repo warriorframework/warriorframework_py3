5ecf5d371376973a650fbf2e93e634cd099d6ad1
Sanika Kulkarni <sanikaadinath.kulkarni@us.fujitsu.com>
2018-12-12 16:55:09 -0600

    Added cases, suites, assembler, cli_data

---
+++ katana/wapps/cases/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.CasesView.as_view(), name='index'),
+    url(r'^get_list_of_cases/$', views.get_list_of_cases, name='get_list_of_cases'),
+    url(r'^get_file/$', views.get_file, name='get_file'),
+    url(r'^save_file/$', views.save_file, name='save_file'),
+]
