583fdb6bcd148a3c05e66f1fbbfd8aba4287fc03
Margret Tumbokon <margret.tumbokon@us.fujitsu.com>
2019-01-04 08:55:15 -0600

    Update admin site look to match new look

--- katana/wui/core/admin.py
+++ katana/wui/core/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 
 # Customize django admin site
-admin.site.site_header = 'Warrior Administration'
+admin.site.site_header = 'WARRIOR ADMINISTRATION'
 admin.site.site_title = 'Warrior Administration'
 admin.site.index_title = ''
 
