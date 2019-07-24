548143cfdf5b561992417922668995a2f3d35b3a
rpenjuri <raghavendra.penjuri@us.fujitsu.com>
2019-07-24 09:27:14 -0500

    Project Directory Naming convention changed

--- katana/wapps/projects/views.py
+++ katana/wapps/projects/views.py
@@ -11,25 +11,25 @@ from django.template.loader import render_to_string
 from utils.directory_traversal_utils import join_path, get_parent_dir_path, get_dir_from_path
 from utils.json_utils import read_json_data, read_xml_get_json
 from utils.navigator_util import Navigator
-from wapps.projects.suite_utils.defaults import on_errors, impacts, contexts, runmodes, \
+from wapps.projects.project_utils.defaults import on_errors, impacts, contexts, runmodes, \
     executiontypes, runtypes
-from wapps.projects.suite_utils.verify_suite_file import VerifySuiteFile
+from wapps.projects.project_utils.verify_project_file import VerifyProjectFile
 
 navigator = Navigator()
 CONFIG_FILE = join_path(navigator.get_katana_dir(), "config.json")
 APP_DIR = join_path(navigator.get_katana_dir(), "wapps", "projects")
 STATIC_DIR = join_path(APP_DIR, "static", "projects")
-TEMPLATE = join_path(STATIC_DIR, "base_templates", "Untitled.xml")
+TEMPLATE = join_path(STATIC_DIR, "base_templates", "Project_Template.xml")
 DROPDOWN_DEFAULTS = read_json_data(join_path(STATIC_DIR, "base_templates", "dropdowns_data.json"))
 
 
 class projectsView(View):
-    print("project View")
+
     def get(self, request):
         """
         Get Request Method
         """
-        return render(request, 'projects/projects.html')
+        return render(reqest, 'projects/projects.html')
 
 
 def get_list_of_suites(request):
@@ -41,18 +41,17 @@ def get_list_of_suites(request):
 
 
 def get_file(request):
-    print("project")
+
     """
     Reads a file, validates it, computes the HTML and returns it as a JSON response
     """
     file_path = request.GET.get('path')
     if file_path == "false":
         file_path = TEMPLATE
-    vcf_obj = VerifySuiteFile(TEMPLATE, file_path)
+    vcf_obj = VerifyProjectFile(TEMPLATE, file_path)
 
     output, data = vcf_obj.verify_file()
-    
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@22")
+
     if output["status"]:
         mid_req = (len(data["Project"]["Requirements"]["Requirement"]) + 1) / 2
         if file_path == TEMPLATE:
@@ -60,7 +59,7 @@ def get_file(request):
         else:
             output["filepath"] = get_parent_dir_path(file_path)
         output["filename"] = os.path.splitext(get_dir_from_path(file_path))[0]
-        print("*********************")
+
         print(output)
         output["html_data"] = render_to_string('projects/display_suite.html',
                                                {"data": data, "mid_req": mid_req,
@@ -74,12 +73,12 @@ def get_file(request):
 
 def save_file(request):
     """ This function saves the file in the given path. """
-    print("___________________________________output data")
+
     output = {"status": True, "message": ""}
     data = json.loads(request.POST.get("data"), object_pairs_hook=collections.OrderedDict)
     print(data["Project"])
     data["Project"]["Details"] = validate_details_data(data["Project"]["Details"])
-    data["Project"]["Testsuites"]["Testsuite"] = validate_step_data(data["Project"]["Testsuites"]["Testsuite"])
+    data["Project"]["Testsuites"]["Testsuite"] = validate_suite_data(data["Project"]["Testsuites"]["Testsuite"])
     xml_data = xmltodict.unparse(data, pretty=True)
     directory = request.POST.get("directory")
     filename = request.POST.get("filename")
@@ -104,7 +103,7 @@ def validate_details_data(data):
     return data
 
 
-def validate_step_data(data):
+def validate_suite_data(data):
     """
     Validates steps of the file before saving
     """
