6e8ab744275dd08b7733f06927e6c3dfffe95bb6
vkotakon-warrior <venkatadhri.kotakonda@us.fujitsu.com>
2019-08-02 00:28:05 -0500

    Removed commented code

--- warrior/WarriorCore/common_execution_utils.py
+++ warrior/WarriorCore/common_execution_utils.py
@@ -46,7 +46,7 @@ def append_step_list(step_list, step, value, go_next, mode, tag):
     return step_list
 
 
-def get_step_list(filepath, step_tag, sub_step_tag, randomize=False,):
+def get_step_list(filepath, step_tag, sub_step_tag, randomize=False):
     """
     Takes the location of Testcase/Suite/Project file as input
     Returns a list of all the step/testcase/testsuite elements
