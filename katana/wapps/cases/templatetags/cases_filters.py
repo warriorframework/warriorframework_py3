c11bbd557cd45823bf1377147d48b5eb35586c39
rpenjuri <raghavendra.penjuri@us.fujitsu.com>
2019-09-16 11:02:36 +0530

    Files opening issue fix.

--- katana/wapps/cases/templatetags/cases_filters.py
+++ katana/wapps/cases/templatetags/cases_filters.py
@@ -1,3 +1,4 @@
+import collections
 from django import template
 from wapps.cases.cases_utils.defaults import inverted_on_errors, inverted_contexts, \
     inverted_impacts, inverted_runmodes, inverted_iteration_types
@@ -33,3 +34,23 @@ def convert_impacts(value):
 @register.filter(name='get_attribute')
 def get_attribute(value, attribute_name):
     return value["@" + attribute_name]
+
+@register.filter(name='get_attribute_new')
+def get_attribute_new(value):
+    if type(value) == list :
+        return "list"
+    elif type(value) ==  None:
+        pass
+    elif type(value) == collections.OrderedDict:
+        return "str"
+
+@register.filter(name='check_value_is_available_or_not')
+def check_value_is_available_or_not(value):
+    if type(value) == list :
+        return "list"
+    elif type(value) == None:
+        pass
+    elif type(value) == collections.OrderedDict:
+        for i in value:
+            if "value" in i :
+                return "str"
