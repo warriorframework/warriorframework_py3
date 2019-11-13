bdcecb67c3be90a74c8804558b1ca80b17b8c523
apathapa <anil.pathapati@us.fujitsu.com>
2019-11-12 23:31:19 -0600

    Adding for all operations

--- warrior/Framework/ClassUtils/rest_utils_class.py
+++ warrior/Framework/ClassUtils/rest_utils_class.py
@@ -61,7 +61,10 @@ class WRest(object):
         """
         pNote("Perform a http post", "info")
         try:
-            response = self.req.post(url, data=data, auth=auth, **kwargs)
+            if auth ==(False, False,):
+                response = self.req.post(url, data=data, **kwargs)
+            else:
+                response = self.req.post(url, data=data, auth=auth, **kwargs)
         except Exception as e:
             status, response = self.catch_expection_return_error(e, url)
         else:
@@ -102,7 +105,10 @@ class WRest(object):
         """
         pNote("Perform a http put", "info")
         try:
-            response = self.req.put(url, data=data, auth=auth, **kwargs)
+            if auth == (False, False,):
+                response = self.req.put(url, data=data, **kwargs)
+            else:
+                response = self.req.put(url, data=data, auth=auth, **kwargs)
         except Exception as e:
             status, response = self.catch_expection_return_error(e, url)
         else:
@@ -121,7 +127,10 @@ class WRest(object):
         """
         pNote("Perform a http patch", "info")
         try:
-            response = self.req.patch(url, data=data, auth=auth, **kwargs)
+            if auth == (False, False, ):
+                response = self.req.patch(url, data=data, **kwargs)
+            else:
+                response = self.req.patch(url, data=data, auth=auth, **kwargs)
         except Exception as e:
             status, response = self.catch_expection_return_error(e, url)
         else:
@@ -140,7 +149,10 @@ class WRest(object):
         """
         pNote("Perform a http delete", "info")
         try:
-            response = self.req.delete(url, auth=auth, **kwargs)
+            if auth == (False, False):
+                response = self.req.delete(url,**kwargs)
+            else:
+                response = self.req.delete(url, auth=auth, **kwargs)
         except Exception as e:
             status, response = self.catch_expection_return_error(e, url)
         else:
@@ -159,7 +171,10 @@ class WRest(object):
         """
         pNote("Perform a http options", "info")
         try:
-            response = self.req.options(url, auth=auth, **kwargs)
+            if auth== (False, False,):
+                response = self.req.options(url, **kwargs)
+            else:
+                response = self.req.options(url, auth=auth, **kwargs)
         except Exception as e:
             status, response = self.catch_expection_return_error(e, url)
         else:
@@ -178,7 +193,10 @@ class WRest(object):
         """
         pNote("Perform a http head", "info")
         try:
-            response = self.req.head(url, auth=auth, **kwargs)
+            if auth == (False, False,):
+                response = self.req.head(url, **kwargs)
+            else:
+                response = self.req.head(url, auth=auth, **kwargs)
         except Exception as e:
             status, response = self.catch_expection_return_error(e, url)
         else:
