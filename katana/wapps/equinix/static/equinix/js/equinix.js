ca789b4d8443c6c9ca6042caefa74e48306ee506
terrakom <manjunath.ko@terralogic.com>
2020-01-21 11:24:09 +0530

    UI changes

--- katana/wapps/equinix/static/equinix/js/equinix.js
+++ katana/wapps/equinix/static/equinix/js/equinix.js
@@ -1,92 +1,71 @@
-window.error_flag = "false";
 var equinix = {
-   set_func: function(){
-    console.log("setting values...!")
-    $(".set_result").val("");
-    data = $(".set_data").val()
+
+measure_func: function(){
+    $(".result").attr("placeholder", "fetching result...");
+    if($(".api_res").css("display") == "none"){
+        $(".api_res").css("display", "block");
+    }
+    if($(".loader").css("display") == "none"){
+        $(".loader").css("display", "block");
+    }
+    $(".set_btn").attr("disabled", true);
+    $(".msr_btn").attr("disabled", true);
+   $odi = $(".msr_odi").val()
+   $msr_min_freq = $(".msr_min_freq").val()
+   $msr_max_freq = $(".msr_max_freq").val()
+   $pr_type = $(".msr_pr_type").find(":selected").text()
+   console.log($odi, $msr_min_freq, $msr_max_freq, $pr_type)
+   $.ajax({
+    headers: {
+        'X-CSRFToken': katana.$activeTab.find('input[name="csrfmiddlewaretoken"]').attr('value')
+    },
+    type: 'GET',
+    url: 'equinix/measure/',
+    dataType: "json",
+    async:false,
+    data: {"odi": $odi, "msr_min_freq":$msr_min_freq, "msr_max_freq" :$msr_max_freq, "pr_type":$pr_type}
+}).done(function(data){
     console.log(data)
-    // try {
-    //     JSON.parse(data);
-    // } catch (e) {
-    //    error_flag = "true"
-    // }
-    // if (error_flag == "true"){
-    //     console.log("invalid");
-    // }
-    // else{
-    //     console.log("valid");
-    // }
-    if (data.trim() === ""){
-        $(".set_data").css("border-color", "red");
-    }
-    else{
-        $(".set_data").css("border-color", "#b1b6bd");
-        if($(".set-result").css("display") == "none"){
-        $(".set-result").css("display", "block");
-        }
-        if($(".set_loader").css("display") == "none"){
-            $(".set_loader").css("display", "block");
-            }
-        $('.set-measure-btn').attr('disabled',true);
-        $.ajax({
-            headers: {
-                'X-CSRFToken': katana.$activeTab.find('input[name="csrfmiddlewaretoken"]').attr('value')
-            },
-            type: 'POST',
-            url: 'equinix/set/',
-            dataType: "json",
-            async:false,
-            data: {"json_data": data}
-        }).done(function(data){
-            if (data["message"] != undefined){
-            $(".set_result").val("Script status: "+data["script_status"]+"\n\n"+"Message: "+data["message"]);
-            }
-            else{
-           $(".set_result").val("Script status: "+data["script_status"]);
-            }
-            $(".set_loader").css("display", "none");
-            $('.set-measure-btn').attr('disabled',false);
-        })
-    }
-   },
+    $(".loader").css("display", "none");
+    $(".result").val(JSON.stringify(data))
+    $(".set_btn").attr("disabled", false);
+    $(".msr_btn").attr("disabled", false);
 
-   measure_func: function(){
-    console.log("measuring values...!");
-    $(".measure_result").val("");
-    data = $(".measure_data").val();
-    if (data.trim() === ""){
-        $(".measure_data").css("border-color", "red");
+})
+},
 
+set_func: function(){
+    $(".result").attr("placeholder", "fetching result...");
+    if($(".api_res").css("display") == "none"){
+        $(".api_res").css("display", "block");
     }
-    else{
-        console.log(data);
-        $(".measure_data").css("border-color", "#b1b6bd");
-        if($(".measure-result").css("display") == "none"){
-        $(".measure-result").css("display", "block");
-        }
-        if($(".measure_loader").css("display") == "none"){
-            $(".measure_loader").css("display", "block");
-            }
-        $('.set-measure-btn').attr('disabled',true);
-        $.ajax({
-            headers: {
-                'X-CSRFToken': katana.$activeTab.find('input[name="csrfmiddlewaretoken"]').attr('value')
-            },
-            type: 'POST',
-            url: 'equinix/measure/',
-            dataType: "json",
-            async:false,
-            data: {"json_data": data}
-        }).done(function(data){
-            if (data["message"] != undefined){
-            $(".measure_result").val("Script status: "+data["script_status"]+"\n\n"+"Message: "+data["message"]);
-            }
-            else{
-                $(".measure_result").val("Script status: "+data["script_status"]);
-            }
-            $('.set-measure-btn').attr('disabled',false);
-            $(".measure_loader").css("display", "none");
-        })
+    if($(".loader").css("display") == "none"){
+        $(".loader").css("display", "block");
     }
-   },
+    $(".set_btn").attr("disabled", true);
+    $(".msr_btn").attr("disabled", true);
+   $odi = $(".set_odi").val()
+   $set_min_freq = $(".set_min_freq").val()
+   $set_max_freq = $(".set_max_freq").val()
+   $set_pr_type = $(".set_pr_type").find(":selected").text()
+   console.log($odi, $set_min_freq, $set_max_freq, $set_pr_type)
+   $.ajax({
+    headers: {
+        'X-CSRFToken': katana.$activeTab.find('input[name="csrfmiddlewaretoken"]').attr('value')
+    },
+    type: 'GET',
+    url: 'equinix/set/',
+    dataType: "json",
+    async:false,
+    data: {"odi": $odi, "set_min_freq":$set_min_freq, "set_max_freq" :$set_max_freq, "pr_type":$set_pr_type}
+}).done(function(data){
+    console.log(data)
+    $(".loader").css("display", "none");
+    $(".result").val(JSON.stringify(data))
+    $(".set_btn").attr("disabled", false);
+    $(".msr_btn").attr("disabled", false);
+
+})
+}
+
 }
--- katana/wapps/equinix/views.py
+++ katana/wapps/equinix/views.py
@@ -18,7 +18,9 @@ from katana.wapps.cases.cases_utils.defaults import impacts, on_errors, runmodes
 from katana.wapps.cases.cases_utils.get_drivers import GetDriversActions
 from katana.wapps.cases.cases_utils.verify_case_file import VerifyCaseFile
 
-
+navigator = Navigator()
+equinix_measure_data_json_path = join_path(navigator.get_katana_dir(), "wapps/equinix/equinix_measure_data.json")
+equinix_set_data_json_path = join_path(navigator.get_katana_dir(), "wapps/equinix/equinix_set_data.json")
 class EquinixView(View):
 
     def get(self, request):
@@ -28,13 +30,48 @@ class EquinixView(View):
         return render(request, 'equinix/equinix_home.html')
 
 def set_api(request):
-    json_string = request.POST.get('json_data')
-    json_data = json.loads(json_string)
-    response =  requests.post('http://0.0.0.0:5002/set', json=json_data)
+    otsi_interface_name = request.GET.get("odi")
+    set_min_freq = request.GET.get("set_min_freq")
+    set_max_freq = request.GET.get("set_max_freq")
+    path_type = request.GET.get("pr_type")
+    equinix_json_data = read_json_data(equinix_set_data_json_path)
+    # print(equinix_json_data)
+    # import pdb; pdb.set_trace()
+    # equinix_json_data[]
+    devices = equinix_json_data["set"].keys()
+    for device in devices:
+        equinix_json_data["set"][device]["interface_name"] = otsi_interface_name
+        equinix_json_data["set"][device]["preset_work_max_frequency"] = set_max_freq
+        equinix_json_data["set"][device]["preset_protect_max_frequency"] = set_max_freq
+        equinix_json_data["set"][device]["min_frequency_preset_work"] = set_min_freq
+        equinix_json_data["set"][device]["min_frequency_preset_protect"] = set_min_freq
+        equinix_json_data["set"][device]["path_type"] = path_type
+    with open(equinix_set_data_json_path, "w") as f:
+        json.dump(equinix_json_data, f)
+    # print(read_json_data(equinix_set_data_json_path))
+    response = requests.post('http://0.0.0.0:5002/measure', json=read_json_data(equinix_set_data_json_path))
     return HttpResponse(response)
 
 def measure_api(request):
-    json_string = request.POST.get('json_data')
-    json_data = json.loads(json_string)
-    response = requests.post('http://0.0.0.0:5002/measure', json=json_data)
+    otsi_interface_name = request.GET.get("odi")
+    msr_min_freq = request.GET.get("msr_min_freq")
+    msr_max_freq = request.GET.get("msr_max_freq")
+    path_type = request.GET.get("pr_type")
+    equinix_json_data = read_json_data(equinix_measure_data_json_path)
+    # print(equinix_json_data)
+    # import pdb; pdb.set_trace()
+    # equinix_json_data[]
+    devices = equinix_json_data["set"].keys()
+    for device in devices:
+        equinix_json_data["set"][device]["interface_name"] = otsi_interface_name
+        equinix_json_data["set"][device]["preset_work_max_frequency"] = msr_max_freq
+        equinix_json_data["set"][device]["preset_protect_max_frequency"] = msr_max_freq
+        equinix_json_data["set"][device]["min_frequency_preset_work"] = msr_min_freq
+        equinix_json_data["set"][device]["min_frequency_preset_protect"] = msr_min_freq
+        equinix_json_data["set"][device]["path_type"] = path_type
+    with open(equinix_measure_data_json_path, "w") as f:
+        json.dump(equinix_json_data, f)
+    # print(otsi_interface_name, msr_min_freq, msr_max_freq, path_type )
+    # print(read_json_data(equinix_measure_data_json_path))
+    response = requests.post('http://0.0.0.0:5002/measure', json=read_json_data(equinix_measure_data_json_path))
     return HttpResponse(response)
