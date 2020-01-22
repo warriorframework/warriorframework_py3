245d0fc2c79739c7172bea306dcd27adbfc86c56
rpenjuri <Raghavendra.Penjuri@us.fujitsu.com>
2020-01-22 04:57:34 -0600

    App configuration implementation and removed all apps

--- katana/manage.py
+++ katana/manage.py
@@ -3,6 +3,25 @@ import os
 import sys
 from os.path import abspath, dirname
 
+try:
+    import katana
+
+    os.environ["pipmode"] = "True"
+# except ModuleNotFoundError as error:
+except:
+    WARRIORDIR = dirname(dirname(abspath(__file__)))
+    sys.path.append(WARRIORDIR)
+    try:
+        import katana
+
+        os.environ["pipmode"] = "False"
+    except:
+        raise
+from katana.utils.navigator_util import Navigator
+from katana.primary_process import install_default_apps
+from katana.utils.json_utils import read_json_data
+from katana.utils.directory_traversal_utils import join_path
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "katana.wui.settings")
     try:
@@ -20,16 +39,23 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-    try:
-        import katana
-        os.environ["pipmode"]= "True"
-    #except ModuleNotFoundError as error:
-    except:
-        WARRIORDIR = dirname(dirname(abspath(__file__)))
-        sys.path.append(WARRIORDIR)
-        try:
-            import katana
-            os.environ["pipmode"]= "False"
-        except:
-            raise
+    nav_obj = Navigator()
+    BASE_DIR = nav_obj.get_katana_dir()
+    app_config_file = BASE_DIR + "/app_config.json"
+    def read_config_file_data():
+            nav_obj = Navigator()
+            config_file_path = join_path(nav_obj.get_katana_dir(), "app_config.json")
+            data = read_json_data(config_file_path)
+            return data
+    app_config_data = read_config_file_data()
+    if app_config_data["__userconfigured__"] == "False" and app_config_data["__normal_run__"] == "False":
+        install_default_apps()
+        app_config_data["__normal_run__"] == "True"
+
+    if app_config_data["__userconfigured__"] == "True":
+        if os.path.exists("log.txt"):
+            print("=============================KATANA CONFIGURATION LOG================================")
+            os.system("tail -n +3 log.txt")
+            print("======================================================================================")
+            print("\n\n")
     execute_from_command_line(sys.argv)
--- katana/native/wapp_management/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    docfile = forms.FileField()
