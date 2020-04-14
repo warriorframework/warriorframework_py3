464d06b82c23c263cf5702edb5f5b8f79a4977d4
rpenjuri <raghavendra.penjuri@us.fujitsu.com>
2020-04-14 01:50:06 -0500

    Added psutil package to setup file.

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
                       "cloudshell-automation-api==9.3.0.175525",
                       "pycryptodome==3.6.1", "Django==2.1.2", "xmltodict==0.12.0",
                       "python-ldap", "django_auth_ldap",
-                      "djangorestframework==3.10.3", "gcg==0.2.0", "termcolor==1.1.0",]
+                      "djangorestframework==3.10.3", "gcg==0.2.0", "termcolor==1.1.0", "psutil==5.7.0",]
 
 )
