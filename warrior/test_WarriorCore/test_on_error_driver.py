20da6f448bbe8db90be60e8d40e7674d7e6419f0
vkotakon-warrior <venkatadhri.kotakonda@us.fujitsu.com>
2020-04-22 17:08:49 +0530

    removed the commented code from file

--- warrior/test_WarriorCore/test_on_error_driver.py
+++ warrior/test_WarriorCore/test_on_error_driver.py
@@ -18,20 +18,11 @@ try:
     import warrior
     # except ModuleNotFoundError as error:
 except Exception as e:
-    # import pdb
-    # pdb.set_trace()
     WARRIORDIR = dirname(dirname(dirname(abspath(__file__))))
     sys.path.append(WARRIORDIR)
     import warrior
 
-# sys.modules['warrior.WarriorCore.Classes.war_cli_class'] = MagicMock()
-
 from warrior.WarriorCore import onerror_driver
-#
-# warrior.Framework.Utils = MagicMock()
-# warrior.Framework.Utils.print_Utils = MagicMock()
-#
-
 
 def test_execute_and_resume():
     """returns ABORT_AS_ERROR for on_error action = abort_as_error """
@@ -41,7 +32,6 @@ def test_execute_and_resume():
     error_handle = {}
     status = onerror_driver.execute_and_resume(action, value, error_handle)
     assert status["action"]  == "EXECUTE_AND_RESUME"
-
 
 def test_abortAsError():
     """returns ABORT_AS_ERROR for on_error action = abort_as_error """
@@ -70,8 +60,6 @@ def test_goto():
     assert status["action"] == "GOTO"
     assert status["value"] == 3
 
-
-
 def test_next():
     """returns 'NEXT' for on_error action = next """
     action = 'next'
@@ -81,72 +69,3 @@ def test_next():
     status = onerror_driver.next(action, value, error_handle, skip_invoked=True)
     assert status["action"] == "NEXT"
 
-# def test_get_failure_results1():
-#     """ Returns the appropriate values based on the onError actions for failing steps.
-#
-#     Arguments:
-#     1. error_repository    = (dict) dictionary containing the onError action, values
-#     """
-#
-#     error_repository = {'action':'NEXT'}
-#
-#     onerror_driver.get_failure_results(error_repository)
-#
-# def test_get_failure_results2():
-#     """ Returns the appropriate values based on the onError actions for failing steps.
-#
-#     Arguments:
-#     1. error_repository    = (dict) dictionary containing the onError action, values
-#     """
-#
-#     error_repository = {'action':'GOTO', 'value':1}
-#
-#     onerror_driver.get_failure_results(error_repository)
-#
-# def test_get_failure_results3():
-#     """ Returns the appropriate values based on the onError actions for failing steps.
-#
-#     Arguments:
-#     1. error_repository    = (dict) dictionary containing the onError action, values
-#     """
-#
-#     error_repository = {'action':'ABORT'}
-#
-#     onerror_driver.get_failure_results(error_repository)
-#
-#
-# def test_getErrorHandlingParameters():
-#     """Takes a xml element at input and returns the values for on_error action , value
-#     If no value is available in the node then returns the default values """
-#
-#     def_on_error_action = ''
-#     def_on_error_value = ''
-#     exec_type = 'yes'
-#
-#     class node(object):
-#         """docstring for node"""
-#         def __init__(self, arg):
-#             super(node, self).__init__()
-#             self.arg = arg
-#
-#         def find(self):
-#             attrib = 'some value'
-#             # return 'yes'
-#     class exec_node(object):
-#         """docstring for node"""
-#         def __init__(self, arg):
-#             super(node, self).__init__()
-#             self.arg = arg
-#
-#         def find(self):
-#             attrib = 'some value'
-# # if exec_type:
-# #     def_on_error_action = 'NEXT'
-# #     def_on_error_value = ''
-# #     ex_rule_param = exec_node.find('Rule').attrib
-# #     action = ex_rule_param['Else']
-# #     if ex_rule_param['Else'].upper() == 'GOTO':
-# #         value = ex_rule_param['Elsevalue']
-#
-#     onerror_driver.getErrorHandlingParameters(node, def_on_error_action, def_on_error_value,\
-#         exec_type, current_step_number=None)
