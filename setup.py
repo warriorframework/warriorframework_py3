a7f013bde9c8a2fc7ba668b631f4f1de89d6f8c6
mkanakat-warrior <manjunath.kanakatte@fujitsu.com>
2020-08-06 01:37:28 -0700

    added mongodb driver

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
                       "cloudshell-automation-api==9.3.0.175525",
                       "pycryptodome==3.6.1", "xmltodict==0.12.0",
                       "xlrd==1.2.0", "openpyxl==3.0.3", "pandas==1.0.3",
-                      "gcg==0.2.0", "termcolor==1.1.0",]
+                      "gcg==0.2.0", "termcolor==1.1.0", "pymongo==3.11.0",]
 
 )
