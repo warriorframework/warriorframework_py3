815315da7d59e3bc5f355c7261eb50fc4680626d
mkanakat-warrior <manjunath.kanakatte@fujitsu.com>
2021-02-25 13:09:28 +0530

    added warrior modules

---
+++ warrior_modules/warrior_kafka_driver/setup.py
@@ -0,0 +1,39 @@
+'''
+Copyright 2017, Fujitsu Network Communications, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "warriorkafka"
+PACKAGE_VERSION = "1.0.0"
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    author="warriorteam",
+    author_email='frameworkwarrior@gmail.com',
+    scripts=[],
+    packages=find_packages(),
+    package_data={'':['**/*', '*']},
+    include_package_data=True,
+    long_description= "kafka package for warrior framework",
+    description="Warrior Framework is an open source Automation Framework",
+    url="https://github.com/warriorframework/warriorframework_py3",
+    project_urls={
+        "Documentation": "http://warriorframework.org/",
+        "Source Code": "https://github.com/warriorframework/warriorframework_py3",
+    },
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python :: 3.6',],
+    install_requires=["kafka-python==1.4.6"]
+
+)
