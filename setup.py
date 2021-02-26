16c07b21be382b333ddc07814f9c2c734fa7ae74
Nayak, Santhosh <santhosh.nayak@us.fujitsu.com>
2021-02-25 23:06:05 -0600

    Update the release notes/setup.py/version.txt  for warrior-framework 4.4.2 Release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ limitations under the License.
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "warriorframework"
-PACKAGE_VERSION = "4.4.0"
+PACKAGE_VERSION = "4.4.2"
 
 setup(
     name=PACKAGE_NAME,
